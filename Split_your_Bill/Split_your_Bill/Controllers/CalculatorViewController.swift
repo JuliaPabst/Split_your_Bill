89f1c57ea57d5d4484a4c2121d6c56be6c09b9e0
Jules <ju.pabst@gmail.com>
2024-01-09 19:59:47 +0100

    created design and created resultsViewController

---
+++ Split_your_Bill/Split_your_Bill/Controllers/CalculatorViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Split_your_Bill
+//
+//  Created by Julia Pabst on 09.01.24.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet var billTextField: UITextField!
+    @IBOutlet var zeroButton: UIButton!
+    @IBOutlet var tenButton: UIButton!
+    @IBOutlet var twentyButton: UIButton!
+    @IBOutlet var splitNumberLabel: UILabel!
+
+    @IBAction func tipChanged(_ sender: UIButton) {
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
+    
+}
+
---
+++ Split_your_Bill/Split_your_Bill/Controllers/ResultsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ResultsViewController.swift
+//  Split_your_Bill
+//
+//  Created by Julia Pabst on 09.01.24.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet var totalLabel: UILabel!
+    
+    @IBOutlet var settingsLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
