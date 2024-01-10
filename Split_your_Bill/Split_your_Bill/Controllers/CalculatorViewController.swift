d2f3252d88998700af0b51b95f0712e074424c07
Jules <ju.pabst@gmail.com>
2024-01-10 17:06:02 +0100

    made textfield close when enter is pressed and made keyboard for textfield numeric

--- Split_your_Bill/Split_your_Bill/Controllers/CalculatorViewController.swift
+++ Split_your_Bill/Split_your_Bill/Controllers/CalculatorViewController.swift
@@ -7,37 +7,54 @@
 
 import UIKit
 
-class CalculatorViewController: UIViewController {
+class CalculatorViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet var billTextField: UITextField!
     @IBOutlet var zeroButton: UIButton!
     @IBOutlet var tenButton: UIButton!
     @IBOutlet var twentyButton: UIButton!
     @IBOutlet var splitNumberLabel: UILabel!
+    
+    @IBOutlet var stepper: UIStepper!
+    
+    @IBOutlet var textfield: UITextField!
+    
+    var tip = 0.0
 
+    override func viewDidLoad() {
+        stepper.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+        textfield.delegate = self
+        textfield.keyboardType = .numberPad
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
     @IBAction func tipChanged(_ sender: UIButton) {
+        zeroButton.isSelected = false
+        tenButton.isSelected = false
+        twentyButton.isSelected = false
         sender.isSelected = true
-        print(sender.tag)
         
         if(sender.tag == 0){
-            print(0.0)
-            tenButton.isSelected = false
-            twentyButton.isSelected = false
+            tip = 0.0
         } else if (sender.tag == 1){
-            print(0.1)
-            zeroButton.isSelected = false
-            twentyButton.isSelected = false
+            tip = 0.1
         } else {
-            print(0.2)
-            zeroButton.isSelected = false
-            tenButton.isSelected = false
+            tip = 0.2
         }
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = String(Int(sender.value))
+        
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        
+        print(textfield.text ?? "No text")
     }
     
 }
